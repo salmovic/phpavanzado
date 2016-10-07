cb27bb58deca0f88c6b778dff72aa6ca7d51093a
Aquiles <mv.salvador93@gmail.com>
2016-10-06 19:50:11 -0500

    capitulo3
    Estructura y Composer

---
+++ core/core.php
@@ -0,0 +1,17 @@
+<?php
+/*
+NUCLEO DE LA APLICACION llamamos a todos los recursos de la aplicacion
+*/
+
+// Se definen las constantes
+// ruta raiz
+define('HTML_DIR','html/');
+// Nombre de la aplicacion
+define('APP_TITLE','OcrendBB Hol');
+// Pie de pagina
+define( 'FOOTER_COPY', 'Copy &copy; '.date( 'Y' ).' Salvador' );
+//url por defecto
+define('APP_URL', 'http://localhost/phpalextremo/php_avanzado/');
+//cargar todas las librerias... [vendor]
+require('vendor/autoload.php');
+ ?>
